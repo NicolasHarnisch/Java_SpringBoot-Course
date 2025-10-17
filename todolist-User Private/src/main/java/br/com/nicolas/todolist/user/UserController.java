c2e01b3396adc77eaace718474086bedd0022b34
NicolasHarnisch <nicolasgomeshar@gmail.com>
2025-10-17 18:41:02 -0300

    Adiciona o projeto todolist-User Private

---
+++ todolist-User Private/src/main/java/br/com/nicolas/todolist/user/UserController.java
@@ -0,0 +1,38 @@
+package br.com.nicolas.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    /**
+     * Modificador
+     * public
+     * private
+     * protected
+     */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiro
+     * Double (double) numeros 0.0000
+     * Char (A C )
+     * Date (data)
+     * Void
+     */
+
+     /**
+      * Body
+      */
+
+    @PostMapping("/")
+    public void creat(@RequestBody UserModel userModel) {
+        System.out.println(userModel.getName());
+
+    }
+    
+}
---
+++ todolist-User Private/src/main/java/br/com/nicolas/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String passaword;
+
+    
+}
