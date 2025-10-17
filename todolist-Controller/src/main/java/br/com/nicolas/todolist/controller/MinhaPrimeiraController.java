86ff85e99d874157c7b8d8e1c4126035b9116855
NicolasHarnisch <nicolasgomeshar@gmail.com>
2025-10-17 17:01:38 -0300

    fix: Adiciona arquivos do projeto todolist-Controller

---
+++ todolist-Controller/src/main/java/br/com/nicolas/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,25 @@
+package br.com.nicolas.todolist.controller; 
+
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController //
+@RequestMapping("/primeiraRota") //
+public class MinhaPrimeiraController { //
+
+    /**
+     * Métodos de acesso do HTTP
+     * GET    - Buscar uma informação
+     * POST   - Adicionar um dado/informação
+     * PUT    - Alterar um dado/info
+     * DELETE - Remover um dado
+     * PATCH  - Alterar somente uma parte da info/dado
+     */
+
+    // Método (funcionalidade) de uma classe
+    @GetMapping("/primeiroMetodo") //
+    public String primeiraMensagem() { //
+        return "Funcionou"; //
+    }
+}
